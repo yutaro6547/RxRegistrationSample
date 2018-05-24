22d7994d60348b1f348401be5f6270126ede3d7d
zukkey <y_vkwi_27_hunhun@yahoo.co.jp>
2018-05-24 09:52:42 +0900

    DataBinding対応

--- app/src/main/java/com/example/suzukihiroshitarou/rxregistrationsample/MainActivity.kt
+++ app/src/main/java/com/example/suzukihiroshitarou/rxregistrationsample/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.suzukihiroshitarou.rxregistrationsample
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.suzukihiroshitarou.rxregistrationsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
